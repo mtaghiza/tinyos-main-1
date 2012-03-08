2faafd51ac0a5ced52b4b12acfb67e40746b2b95
Doug Carlson <carlson@cs.jhu.edu>
2012-03-08 12:57:23 -0500

    Subtract mean

--- apps/breakfast/bacon/testCXFlood/scripts/framing.R
+++ apps/breakfast/bacon/testCXFlood/scripts/framing.R
@@ -18,13 +18,15 @@ source('scripts/basicPlots.R')
 framingData <- read.csv(framingDataFile, col.names='Offset')
 framingData$Offset <- framingData$Offset * 1000000
 
+framingData$Offset <- framingData$Offset - mean(framingData$Offset)
+
 allOffsets <- rbind(framingData)
 minOffset <- min(allOffsets$Offset)
 maxOffset <- max(allOffsets$Offset)
 q90 <- quantile(allOffsets$Offset, c(0.9))
 q10 <- quantile(allOffsets$Offset, c(0.1))
 
-plotData(framingData, "Frame Length", outPrefix, "full.framing",
+plotData(framingData, "Frame Length w.r.t Mean", outPrefix, "full.framing",
   minOffset, maxOffset, "(uS)")
-plotData(framingData, "Frame Length", outPrefix, "q80.framing", q10,
+plotData(framingData, "Frame Length w.r.t Mean", outPrefix, "q80.framing", q10,
   q90, "(uS)")
