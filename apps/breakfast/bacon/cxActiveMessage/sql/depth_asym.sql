83508d428be491fafcd3daffdb4e49dcebc00413
Doug Carlson <carlson@cs.jhu.edu>
2012-08-06 18:50:09 -0400

    better output

--- apps/breakfast/bacon/cxActiveMessage/sql/depth_asym.sql
+++ apps/breakfast/bacon/cxActiveMessage/sql/depth_asym.sql
@@ -4,11 +4,13 @@ SELECT
   sd.avgDepth as dist_sd,
   sd.sdDepth as sdev_sd,
   ds.avgDepth as dist_ds,
-  sd.sdDepth as sdev_ds,
-  abs(sd.avgDepth - ds.avgDepth) as diff
+  ds.sdDepth as sdev_ds,
+  abs(sd.avgDepth - ds.avgDepth) as absDiff,
+  sd.avgDepth - ds.avgDepth as diff,
+  sd.sdDepth - ds.sdDepth as sd_diff
 FROM agg_depth sd
 JOIN agg_depth ds 
 ON sd.src = ds.dest and sd.dest = ds.src
 WHERE sd.src < sd.dest
 AND sd.src = 0
-ORDER BY diff;
+ORDER BY absDiff;
