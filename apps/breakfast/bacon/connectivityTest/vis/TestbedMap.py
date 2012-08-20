d6b6193c8a7d6848a8f7b3d381f8413c33687a89
Doug Carlson <carlson@cs.jhu.edu>
2012-08-20 15:48:29 -0400

    several new visualizations, cli

--- apps/breakfast/bacon/connectivityTest/vis/TestbedMap.py
+++ apps/breakfast/bacon/connectivityTest/vis/TestbedMap.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import matplotlib.pyplot as plt
 import networkx as nx
+import sqlite3
+import sys
 
 class TestbedMap(object):
     def __init__(self, nsluFile='nslu_locations.txt', 
@@ -46,6 +48,8 @@ class TestbedMap(object):
           nodelist = self.G.nodes()
           , node_color=[self.G.node[n][self.colAttr] for n in self.G.nodes()]
           , cmap = palette
+          , linewidths = [self.G.node[n].get('linewidth', 1.0) 
+              for n in self.G.nodes()]
         )
 
     def drawEdges(self, alpha=0.2):
@@ -66,7 +70,7 @@ class TestbedMap(object):
     def setAttr(self, attr, attrMap, defaultVal=None):
         #ugly: we want to have a way to enforce that every node has a
         #  value in this map.
-        if defaultVal:
+        if defaultVal is not None:
             for n in self.G.nodes():
                 if n not in attrMap:
                     attrMap[n] = defaultVal
@@ -78,24 +82,34 @@ class TestbedMap(object):
     def setLabels(self, labelMap):
         self.labelMap = labelMap
 
-    def draw(self, outFile=None, node_size=200, palette=plt.cm.hot):
+    def addOutlined(self, nodeId, width):
+        self.G.node[nodeId]['linewidth'] = width
+
+    def draw(self, outFile=None, node_size=200, palette=plt.cm.jet):
         self.drawCMapNodes(node_size, palette)
         self.drawEdges()
         self.drawLabels()
+        self.postDraw()
         if not outFile:
             plt.show()
         else:
             #TODO: file output
             pass
 
+    def postDraw(self):
+        pass
+
 class SingleTXDepth(TestbedMap):
-    def __init__(self, root, edgeFile, prr_threshold=0.0, **kwargs):
+    def __init__(self, root, edgeFile, prr_threshold=0.0,
+            distanceLabels = False, **kwargs):
         super(SingleTXDepth, self).__init__(*kwargs)
         self.addPrrEdges(edgeFile, prr_threshold)
         self.distances = self.computeSPs(root)
         self.setAttr('distance', self.distances)
         self.setColAttr('distance')
-        self.setLabels(self.distances)
+        self.addOutlined(root, 3)
+        if distanceLabels:
+            self.setLabels(self.distances)
 
     def addPrrEdges(self, edgeFile, prr_threshold):
         """ Add edges to a graph from PRR file, where PRR is above some
@@ -130,8 +144,8 @@ class CXDistance(TestbedMap):
         self.setAttr('distance', self.distances)
         self.setColAttr('distance')
 
-        rounded = dict( [ (k, "%.1f"%self.distances[k]) for k in
-          self.distances])
+        rounded = dict( [ (k, "%.1f"%self.distances[k]) 
+          for k in self.distances])
         self.setLabels(rounded)
 
     def loadDistances(self, root, distanceFile, unreachableVal=-1):
@@ -146,12 +160,81 @@ class CXDistance(TestbedMap):
             if nodeId not in self.distances:
                 self.distances[nodeId] = unreachableVal
 
-if __name__ == '__main__':
-    #tbm = SingleTXDepth(0, 'prr_links.csv', 0.95)
-    tbm = CXDistance(0, 'cxDepth.csv')
-    tbm.draw(node_size=300)
+class CXForwarders(TestbedMap):
+    def __init__(self, src, dest, dbFile, **kwargs):
+        super(CXForwarders, self).__init__(*kwargs)
+        self.loadForwarders(src, dest, dbFile)
+        self.fwdRatio[src]=1.0
+        self.fwdRatio[dest]=1.0
+        self.setAttr('fwdRatio', self.fwdRatio, 0)
+        self.setColAttr('fwdRatio')
+        self.addOutlined(src, 3)
+        self.addOutlined(dest, 3)
+        rounded = dict([ (k, "%.2f"%self.fwdRatio[k]) 
+          for k in self.fwdRatio])
+        self.setLabels(rounded)
 
-#TODO: OO-ify
+    def loadForwarders(self, src, dest, dbFile):
+        c = sqlite3.connect(dbFile)
+        self.fwdRatio = dict(c.execute('SELECT fwd, avg(f) FROM routes where src=? and dest=? group by src, fwd, dest', (src, dest)))
+        c.close()
+
+class CXPrr(TestbedMap):
+    def __init__(self, src, dbFile, **kwargs):
+        super(CXPrr, self).__init__(*kwargs)
+        self.loadPrrs(src, dbFile)
+        self.prrs[src]=1.0
+        self.setAttr('prr', self.prrs, 0.0)
+        self.setColAttr('prr')
+        self.addOutlined(src, 3)
+        rounded = dict([ (k, "%.2f"%self.prrs[k]) 
+          for k in self.prrs])
+        self.setLabels(rounded)
+
+    def loadPrrs(self, src, dbFile):
+        c = sqlite3.connect(dbFile)
+        self.prrs = dict(c.execute('SELECT dest, prr from prr_no_startup where src=?', (src,)))
+        c.close()
+
+class SinglePrr(TestbedMap):
+    def __init__(self, src, dbFile, **kwargs):
+        super(SinglePrr, self).__init__(*kwargs)
+        self.loadPrrs(src, dbFile)
+        self.prrs[src]=1.0
+        self.setAttr('prr', self.prrs, 0.0)
+        self.setColAttr('prr')
+        self.addOutlined(src, 3)
+        rounded = dict([ (k, "%.2f"%self.prrs[k]) 
+          for k in self.prrs])
+        self.setLabels(rounded)
+
+    def loadPrrs(self, src, dbFile):
+        c = sqlite3.connect(dbFile)
+        self.prrs = dict(c.execute('SELECT dest, prr from link where src=?', (src,)))
+        c.close()
+        
+if __name__ == '__main__':
+    t = sys.argv[1]
+    if t == '--simple':
+        src = int(sys.argv[2])
+        tbm = SingleTXDepth(src, 'prr_links.csv', 0.95)
+    elif t == '--cxd':
+        tbm = CXDistance(0, 'cxDepth.csv')
+    elif t == '--cxf':
+        src = int(sys.argv[2])
+        dest = int(sys.argv[3])
+        fn = sys.argv[4]
+        tbm = CXForwarders(src, dest, fn)
+    elif t == '--cxp':
+        src = int(sys.argv[2])
+        fn = sys.argv[3]
+        tbm = CXPrr(src, fn)
+    elif t == '--sp':
+        src = int(sys.argv[2])
+        fn = sys.argv[3]
+        tbm = SinglePrr(src, fn)
+    tbm.draw(node_size=400)
+
 # - common internal state: Graph, edges, labels, colors
 # - common behavior: construct, draw (to file or screen)
 # - subclasses define rules for what constitutes edge, label, color.
@@ -159,20 +242,20 @@ if __name__ == '__main__':
 #   - edges are PRR > threshold
 #   - label is depth
 #   - color is depth
-# - CX distance subclass
+# x CX distance subclass
 #   - no edges
 #   - label is depth
 #   - color is depth
-# - CX forwarder rates
+# x CX forwarder rates
 #   - no edges
 #   - color for src, dest fixed
 #   - other nodes: fraction of bursts for which node is forwarder
 #   - label is node id?
-# - CX PRR
+# 0 CX PRR
 #   - no edges
 #   - color is PRR (mark source)
 #   - label is node ID
-# - CX trace
+# 0 CX trace
 #   - no edges
 #   - burst setup info to gray out non-forwarders
 #   - for each frame in transmission, color as:
