6bb5b623d745d2d476edbd6aabf3236e11b583f7
Doug Carlson <carlson@cs.jhu.edu>
2012-09-30 10:56:53 -0400

    adapt for threshold distance stability plots

--- apps/breakfast/bacon/cxAnalysis/fig_scripts/depth_comparison_distribution.R
+++ apps/breakfast/bacon/cxAnalysis/fig_scripts/depth_comparison_distribution.R
@@ -5,9 +5,14 @@ x <- c()
 library(plyr)
 library(ggplot2)
 
+plotTitle <- 'Distance Comparison'
+legendSettings <- 'sim'
+
+
 for (i in seq(argStart, argc-1)){
   opt <- commandArgs()[i]
   val <- commandArgs()[i+1]
+  print(paste(opt, ':', val))
   if ( opt == '--csv'){
     fn <- val
     label <- commandArgs()[i+2]
@@ -15,13 +20,25 @@ for (i in seq(argStart, argc-1)){
     tmp$label <- label
     x <- rbind(x, tmp)
   }
+  if (opt == '--db'){
+    fn <- val
+    label <- commandArgs()[i+2]
+    con <- dbConnect(dbDriver("SQLite"), dbname=fn)
+    tmp <- dbGetQuery(con, selectQ) 
+    tmp$label <- label
+    x <- rbind(x, tmp)
+  }
   if ( opt == '--pdf' ){
     plotFile=T
-    pdf(val, width=12, height=6, title="Sim v. CX depth")
+    pdf(val, width=12, height=6, title=plotTitle)
   }
   if ( opt == '--png' ){
     plotFile=T
     png(val, width=12, height=6, units="in", res=200)
+  }
+  if (opt == '--labels'){
+    print("labels opt")
+    legendSettings <- val
   }
 }
 
@@ -33,20 +50,40 @@ agg <- ddply(x, .(label, dest), summarise,
 
 pd <- position_dodge(0.5)
 agg <- agg[agg$depth > 1,]
-print(ggplot(agg, aes(x=reorder(dest, depth), y=depth, colour=label)) 
-  + geom_point(position=pd)
-  + geom_errorbar(aes(ymin=depth-sd, ymax=depth+sd), width=.1, position=pd) 
-#  + geom_errorbar(aes(ymin=lq, ymax=uq), width=.1, position=pd) 
-  + xlab("Node ID")
-  + ylab("Distance")
-  + scale_colour_hue(name="Dataset", 
-    breaks=c("testbed", "sim_0.4_10_ind_0", "sim_0.4_10_ind_1"),
-    labels=c("Testbed", "Simulation ( + interference)", "Simulation (no interference)"))
-  + ggtitle("Simulation v. Actual Distance")
-  + theme_bw()
-  + theme(legend.justification=c(1,0), legend.position=c(1,0))
-)
 
+
+if ( legendSettings == 'sim'){
+  plotTitle <- "Simulation v. Actual Distance"
+  print(ggplot(agg, aes(x=reorder(dest, depth), y=depth, colour=label)) 
+    + geom_point(position=pd)
+    + geom_errorbar(aes(ymin=depth-sd, ymax=depth+sd), width=.1, position=pd) 
+  #  + geom_errorbar(aes(ymin=lq, ymax=uq), width=.1, position=pd) 
+    + xlab("Node ID")
+    + ylab("Distance")
+    + ggtitle(plotTitle) 
+    + scale_colour_hue(name="Dataset", 
+        breaks=c("testbed", "sim_0.4_10_ind_0", "sim_0.4_10_ind_1"),
+        labels=c("Testbed", "Simulation ( + interference)", "Simulation (no interference)")) 
+    + theme_bw()
+    + theme(legend.justification=c(1,0), legend.position=c(1,0))
+  )
+}
+
+if (legendSettings == 'thresh'){
+  plotTitle <- "Distance v. RSSI Threshold"
+  print(ggplot(agg, aes(x=reorder(dest, depth), y=depth, colour=label)) 
+    + geom_point(position=pd)
+    + geom_errorbar(aes(ymin=depth-sd, ymax=depth+sd), width=.1, position=pd) 
+  #  + geom_errorbar(aes(ymin=lq, ymax=uq), width=.1, position=pd) 
+    + xlab("Node ID")
+    + ylab("Distance")
+    + ggtitle(plotTitle) 
+    + scale_colour_hue(name="Threshold (dBm)")
+    + theme_bw()
+    + theme(legend.justification=c(1,0), legend.position=c(1,0))
+  )
+
+}
 if (plotFile){
   g <- dev.off()
 }
