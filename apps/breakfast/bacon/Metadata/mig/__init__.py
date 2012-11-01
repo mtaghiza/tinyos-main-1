1cd0bd30f5d2398dcd00d20a9d7285feed634e0f
Doug Carlson <carlson@cs.jhu.edu>
2012-11-01 16:51:03 -0400

    python package init

---
+++ apps/breakfast/bacon/Metadata/mig/__init__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+##generated with: 
+## grep 'msg{' ctrl_messages.h | awk '{print $3}' | tr -d '{' | sed -re 's,(_|^)([a-z]),\u\2,g'
+allS='''ReadIvCmdMsg
+ReadIvResponseMsg
+ReadMfrIdCmdMsg
+ReadMfrIdResponseMsg
+ReadBaconBarcodeIdCmdMsg
+ReadBaconBarcodeIdResponseMsg
+WriteBaconBarcodeIdCmdMsg
+WriteBaconBarcodeIdResponseMsg
+ReadToastBarcodeIdCmdMsg
+ReadToastBarcodeIdResponseMsg
+WriteToastBarcodeIdCmdMsg
+WriteToastBarcodeIdResponseMsg
+ReadToastAssignmentsCmdMsg
+ReadToastAssignmentsResponseMsg
+WriteToastAssignmentsCmdMsg
+WriteToastAssignmentsResponseMsg
+ReadBaconTlvCmdMsg
+ReadBaconTlvResponseMsg
+ReadToastTlvCmdMsg
+ReadToastTlvResponseMsg
+WriteBaconTlvCmdMsg
+WriteBaconTlvResponseMsg
+WriteToastTlvCmdMsg
+WriteToastTlvResponseMsg
+DeleteBaconTlvEntryCmdMsg
+DeleteBaconTlvEntryResponseMsg
+DeleteToastTlvEntryCmdMsg
+DeleteToastTlvEntryResponseMsg
+AddBaconTlvEntryCmdMsg
+AddBaconTlvEntryResponseMsg
+AddToastTlvEntryCmdMsg
+AddToastTlvEntryResponseMsg'''
+__all__= allS.split()
