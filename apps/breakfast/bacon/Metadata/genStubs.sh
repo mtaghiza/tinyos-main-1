053c508453d7817b0a6685c0b9377e9d509437b6
Doug Carlson <carlson@cs.jhu.edu>
2012-11-15 15:40:58 -0500

    OK, clean up once again

--- apps/breakfast/bacon/Metadata/genStubs.sh
+++ apps/breakfast/bacon/Metadata/genStubs.sh
@@ -11,25 +11,30 @@ do
   cmdMsg=${ca}_cmd_msg;
   responseMsg=${ca}_response_msg;
 cat <<EOF
-  message_t* ${cmdMsg} = NULL;
-  message_t* ${responseMsg} = NULL;
   task void respond${ca}();
 
   event message_t* ${ca}CmdReceive.receive(message_t* msg_, 
       void* payload,
       uint8_t len){
-    if (${cmdMsg} != NULL){
+    currentCommandType = call AMPacket.type(msg_);
+    if (cmdMsg != NULL){
       printf("RX: ${ca}");
       printf(" BUSY!\n");
       printfflush();
       return msg_;
     }else{
       if ((call Pool.size()) >= 2){
-        message_t* ret = call Pool.get();
-        ${responseMsg} = call Pool.get();
-        ${cmdMsg} = msg_;
-        post respond${ca}();
-        return ret;
+        if (call LastSlave.get() == 0){
+          loadTLVError = EOFF;
+          post handleLoaded();
+          return msg_;
+        }else{
+          message_t* ret = call Pool.get();
+          responseMsg = call Pool.get();
+          cmdMsg = msg_;
+          post respond${ca}();
+          return ret;
+        }
       }else{
         printf("RX: ${ca}");
         printf(" Pool Empty!\n");
@@ -40,19 +45,19 @@ cat <<EOF
   }
 
   task void respond${ca}(){
-    ${cmdType}* commandPl = (${cmdType}*)(call Packet.getPayload(${cmdMsg}, sizeof(${cmdType})));
-    ${responseType}* responsePl = (${responseType}*)(call Packet.getPayload(${responseMsg}, sizeof(${responseType})));
+    ${cmdType}* commandPl = (${cmdType}*)(call Packet.getPayload(cmdMsg, sizeof(${cmdType})));
+    ${responseType}* responsePl = (${responseType}*)(call Packet.getPayload(responseMsg, sizeof(${responseType})));
     //TODO: other processing logic
     responsePl->error = FAIL;
-    call ${ca}ResponseSend.send(0, ${responseMsg}, sizeof(${responseType}));
+    call ${ca}ResponseSend.send(0, responseMsg, sizeof(${responseType}));
   }
 
   event void ${ca}ResponseSend.sendDone(message_t* msg, 
       error_t error){
-    call Pool.put(${responseMsg});
-    call Pool.put(${cmdMsg});
-    ${cmdMsg} = NULL;
-    ${responseMsg} = NULL;
+    call Pool.put(responseMsg);
+    call Pool.put(cmdMsg);
+    cmdMsg = NULL;
+    responseMsg = NULL;
     printf("Response sent\n");
     printfflush();
   }
