57cb3f0132269f273e4924c36d4526f007b66f40
Doug Carlson <carlson@cs.jhu.edu>
2013-02-09 14:49:48 -0500

    function for generating read-analog commands

--- apps/breakfast/bacon/Metadata/md-cli.py
+++ apps/breakfast/bacon/Metadata/md-cli.py
@@ -52,6 +52,61 @@ class Dispatcher:
             m)
         self.sendCount += 1
 
+REFERENCE_AVcc_AVss = 0
+REFERENCE_VREFplus_AVss = 1
+REFERENCE_VeREFplus_AVss = 2
+REFERENCE_AVcc_VREFnegterm = 4
+REFERENCE_VREFplus_VREFnegterm = 5
+REFERENCE_VeREFplus_VREFnegterm = 6
+
+#list of SAMPLE_HOLD_xx_CYCLES constants. the corresponding enum of
+# the i-th element is i.
+sht_enum_vals = [ 4, 8, 16, 32, 64, 96, 128, 192, 256, 384, 512, 768, 1024]
+
+
+def readAnalog(channel, sensorImpedance, warmUpMs = 0,
+  sref = REFERENCE_VREFplus_AVss, ref2_5v = True, samplePeriod32k = 0):
+    m = ReadAnalogSensorCmdMsg()
+    #direct inputs:
+    #input channel
+    #voltage range (sref, ref2_5v)
+    #warm-up time
+    #sample period
+    m.set_inch(channel)
+    m.set_sref(sref)
+    m.set_ref2_5v(ref2_5v)
+    m.set_delayMS(warmUpMs)
+    m.set_samplePeriod(samplePeriod32k)
+
+    #fixed timing values:
+    #sampcon_ssel: 1 (ACLK, 32 binary KHz on toast)
+    #sampcon_id: 0 (/1)
+    #adc12ssel: 3 (SMCLK, 1 binary MHz on toast) 
+    #adc12div: 0 (/1)(1 binary uS/tick)
+    m.set_sampcon_ssel(1)
+    m.set_sampcon_id(0)
+    m.set_adc12ssel(3)
+    m.set_adc12div(0)
+
+    #computed values (from sensorImpedance)
+    #sht
+
+    #sample time from msp430x2xx user guide, 23.2.4.3
+    t_sample = (sensorImpedance*2000)*3.6e-10 + 800e-9
+    #inverse, fyi
+    # r = (t_sample - 800e-9)/3.6e-10 - 2000
+
+    #frequency = 1 binary MHz 
+    smclkTickLen = 1.0/(2**20.0)
+    smclkTicks = t_sample/smclkTickLen
+    valid_sht_enums = [i for (i,v) in enumerate(sht_enum_vals) if v > smclkTicks]
+    if valid_sht_enums:
+        m.set_sht(valid_sht_enums[0])
+    else:
+        raise Exception("Sensor impedance too high: maximum sample-hold-time is 1 binary ms, roughly 2.8M ohm impedance") 
+
+    return m
+
 if __name__ == '__main__':
     packetSource = 'serial@/dev/ttyUSB0:115200'
     destination = 1
