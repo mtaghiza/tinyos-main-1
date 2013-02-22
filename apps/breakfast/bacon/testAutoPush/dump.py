199787d66f34db755e93ac36fa2e57993c15c561
Doug Carlson <carlson@cs.jhu.edu>
2013-02-22 14:10:17 -0500

    python util to split message into records

--- apps/breakfast/bacon/testAutoPush/dump.py
+++ apps/breakfast/bacon/testAutoPush/dump.py
@@ -22,12 +22,44 @@ class PrintfLogger(object):
             mb = mb[:mb.index(0)]
         sys.stdout.write(''.join(chr(v) for v in mb))
 
+
+class RecordIterator(object):
+    COOKIE_LEN = 4
+    LENGTH_LEN = 1
+    
+    def val(self, arr):
+        return reduce(lambda l, r: (l<<8) + r, arr, 0)
+
+    def __init__(self, recordMsg):
+        self.data = recordMsg.get_data()
+        self.si = 0
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        while self.si < len(self.data):
+            cookieBytes = self.data[self.si:self.si+RecordIterator.COOKIE_LEN]
+            self.si += RecordIterator.COOKIE_LEN
+            if cookieBytes == [0xff, 0xff, 0xff, 0xff]:
+                raise StopIteration
+            cookieVal = self.val(cookieBytes)
+            lenVal = self.val(self.data[self.si:self.si+RecordIterator.LENGTH_LEN])
+            self.si += RecordIterator.LENGTH_LEN
+            recordData = self.data[self.si:self.si + lenVal]
+            self.si += lenVal
+            return (cookieVal, lenVal, recordData)
+        raise StopIteration
+
+
 class GenericLogger(object):
     def __init__(self):
         pass
 
     def receive(self, src, msg):
         print msg
+        for record in RecordIterator(msg):
+            print "#",record
 
 
 class Dispatcher:
