2a4d81eb8a37d71cc56f2aca069fa9954251069e
Doug Carlson <carlson@cs.jhu.edu>
2013-05-09 17:50:18 -0400

    adjust settings for overnight test

--- apps/breakfast/bacon/testbed/src/all.tx.sh
+++ apps/breakfast/bacon/testbed/src/all.tx.sh
@@ -7,5 +7,9 @@ then
   ./$sd/blink.sh 
 fi
 
-./$sd/install.sh maps/map.root $od/root.options $od/all.options $od/static.options $od/rx.options
+
 ./$sd/install.sh maps/map.nonroot $od/slave.options $od/all.options $od/static.options $od/broadcast_fast.options
+echo "Hit enter when slaves are confirmed booted"
+read line
+
+./$sd/install.sh maps/map.root $od/root.options $od/all.options $od/static.options $od/rx.options
