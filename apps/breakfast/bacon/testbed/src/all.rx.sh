3e1c01b62b27e6474f66d9bae8695e436d9b783d
Doug Carlson <carlson@cs.jhu.edu>
2013-06-03 13:02:54 -0400

    add label to idle baseline test

--- apps/breakfast/bacon/testbed/src/all.rx.sh
+++ apps/breakfast/bacon/testbed/src/all.rx.sh
@@ -2,11 +2,13 @@
 od=options
 sd=$(dirname $0)
 
+label=$1
+shift 1
 if [ $# -eq 0 ]
 then
   ./$sd/blink.sh 
 fi
 
-source ./$sd/install.sh maps/map.root $od/root.options $od/all.options $od/static.options $od/rx.options $od/radiostats.options
+source ./$sd/install.sh maps/map.root $label $od/root.options $od/all.options $od/static.options $od/rx.options $od/radiostats.options
 
-source ./$sd/install.sh maps/map.nonroot $od/slave.options $od/all.options $od/static.options $od/rx.options $od/radiostats.options
+source ./$sd/install.sh maps/map.nonroot $label $od/slave.options $od/all.options $od/static.options $od/rx.options $od/radiostats.options
