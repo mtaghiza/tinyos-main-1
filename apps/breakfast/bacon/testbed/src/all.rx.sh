de09e5032aa209b5be980d46c18aed719504d3bd
Doug Carlson <carlson@cs.jhu.edu>
2013-06-03 12:55:02 -0400

    add radio logging to idle/static baseline

--- apps/breakfast/bacon/testbed/src/all.rx.sh
+++ apps/breakfast/bacon/testbed/src/all.rx.sh
@@ -7,5 +7,6 @@ then
   ./$sd/blink.sh 
 fi
 
-source ./$sd/install.sh maps/map.root $od/root.options $od/all.options $od/static.options $od/rx.options
-source ./$sd/install.sh maps/map.nonroot $od/slave.options $od/all.options $od/static.options $od/rx.options
+source ./$sd/install.sh maps/map.root $od/root.options $od/all.options $od/static.options $od/rx.options $od/radiostats.options
+
+source ./$sd/install.sh maps/map.nonroot $od/slave.options $od/all.options $od/static.options $od/rx.options $od/radiostats.options
