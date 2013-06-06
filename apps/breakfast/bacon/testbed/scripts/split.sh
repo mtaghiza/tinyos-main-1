5b0ad9958f2882fc16cdc2aee85d46c33c79c3d5
Doug Carlson <carlson@cs.jhu.edu>
2013-06-06 09:57:27 -0400

    handle logs with only one test correctly

--- apps/breakfast/bacon/testbed/scripts/split.sh
+++ apps/breakfast/bacon/testbed/scripts/split.sh
@@ -22,7 +22,12 @@ tf=$(tempfile)
 dos2unix -n $logFile $tf
 startLine=$(grep -n " $rootId START " $tf | head -1 | awk -F ':' '{print $1}')
 endLine=$(grep -n " $rootId START " $tf | head -2 | tail -1 | awk -F ':' '{print $1}')
+if [ "$startLine" == "$endLine" ]
+then
+  endLine=$(wc -l < $tf)
+fi
 
+echo "start: $startLine end: $endLine"
 while [ "$startLine" != "$endLine" -a "$startLine" != "" ]
 do
   of=$outDir/$(head --lines=$startLine $tf | tail -1 | tr '=' ' ' | awk '{
