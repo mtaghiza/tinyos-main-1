7b50668a591b43ab18a03ab187fb77b7e80d8404
Doug Carlson <carlson@cs.jhu.edu>
2013-06-17 15:31:57 -0400

    union serial header in packet (printf still bad)

--- tos/lib/cxl/packet/platform_message.h
+++ tos/lib/cxl/packet/platform_message.h
@@ -3,10 +3,11 @@
 
 #include "CXLink.h"
 #include "Rf1aPacket.h"
+#include "Serial.h"
 
-//TODO: does this need to be a union with serial header?
 typedef union message_header {
   cx_link_header_t cx_link_header;
+  serial_header_t serial_header;
 } __attribute__((packed)) message_header_t;
 
 typedef struct TOSRadioFooter {
