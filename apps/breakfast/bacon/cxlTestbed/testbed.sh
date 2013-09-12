1fa21707a64b0afc420f22f8557fdad4da8dc17a
Doug Carlson <carlson@cs.jhu.edu>
2013-09-12 10:20:13 -0400

    new router for patch 3, set slack in installer

--- apps/breakfast/bacon/cxlTestbed/testbed.sh
+++ apps/breakfast/bacon/cxlTestbed/testbed.sh
@@ -4,7 +4,10 @@ set -x
 
 routerPower=0xC3
 leafPower=0x2D
-enablePrintf=0
+enablePrintf=1
+rxSlack=20UL
+txSlack=100UL
+enableBaconSampler=0
 for map in map.p0 map.p1 map.p2 map.p3
 do
   snc=$(grep SNC $map | cut -d ' ' -f 2)
@@ -14,6 +17,8 @@ do
     SUBNETWORK_CHANNEL=$snc\
     ROUTER_CHANNEL=64\
     ENABLE_AUTOPUSH=1\
+    RX_SLACK=$rxSlack\
+    TX_SLACK=$txSlack\
     ENABLE_PRINTF=$enablePrintf || exit 1
   ./burnRole.sh $map Leaf -f Makefile.dummycxl \
     MAX_POWER=$leafPower\
@@ -21,5 +26,9 @@ do
     SUBNETWORK_CHANNEL=$snc\
     ROUTER_CHANNEL=64\
     ENABLE_AUTOPUSH=1\
+    RX_SLACK=$rxSlack\
+    TX_SLACK=$txSlack\
+    ENABLE_BACON_SAMPLER=$enableBaconSampler\
+    DEFAULT_SAMPLE_INTERVAL=61440UL\
     ENABLE_PRINTF=$enablePrintf || exit 1
 done  
