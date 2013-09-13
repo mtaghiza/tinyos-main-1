a9c2d31502008facf75a4d4d82d298b02f16e8f1
Doug Carlson <carlson@cs.jhu.edu>
2013-09-13 11:23:12 -0400

    put new compile flags into makefiles/scripts

--- apps/breakfast/bacon/cxlTestbed/testbed.sh
+++ apps/breakfast/bacon/cxlTestbed/testbed.sh
@@ -7,7 +7,8 @@ leafPower=0x2D
 enablePrintf=1
 rxSlack=20UL
 txSlack=100UL
-enableBaconSampler=0
+enableBaconSampler=1
+enableForwarderSelection=0
 for map in map.p0 map.p1 map.p2 map.p3
 do
   snc=$(grep SNC $map | cut -d ' ' -f 2)
@@ -19,6 +20,7 @@ do
     ENABLE_AUTOPUSH=1\
     RX_SLACK=$rxSlack\
     TX_SLACK=$txSlack\
+    ENABLE_FORWARDER_SELECTION=$enableForwarderSelection\
     ENABLE_PRINTF=$enablePrintf || exit 1
   ./burnRole.sh $map Leaf -f Makefile.dummycxl \
     MAX_POWER=$leafPower\
@@ -28,6 +30,7 @@ do
     ENABLE_AUTOPUSH=1\
     RX_SLACK=$rxSlack\
     TX_SLACK=$txSlack\
+    ENABLE_FORWARDER_SELECTION=$enableForwarderSelection\
     ENABLE_BACON_SAMPLER=$enableBaconSampler\
     DEFAULT_SAMPLE_INTERVAL=61440UL\
     ENABLE_PRINTF=$enablePrintf || exit 1
