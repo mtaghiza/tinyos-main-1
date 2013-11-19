b835d170216e9911e4ee3b5e08adfa5df43d41f9
Doug Carlson <carlson@cs.jhu.edu>
2013-11-19 17:03:36 -0500

    add function to get biggest missing block for node

--- apps/breakfast/tools/Life/tools/cx/db/DatabaseMissing.py
+++ apps/breakfast/tools/Life/tools/cx/db/DatabaseMissing.py
@@ -35,6 +35,19 @@ class DatabaseMissing(object):
       AND l.retry < 5
       ORDER BY l.node_id, missing desc'''
 
+    MISSING_ORDER_SIZE_NODE_SQL = '''SELECT l.node_id, 
+      l.cookie, l.nextCookie,
+      (r.cookie - l.nextCookie -1) as missing,
+      l.retry
+    FROM sorted_flash l
+    JOIN sorted_flash r
+    ON l.node_id = r.node_id
+      AND l.ROWID +1 = r.ROWID
+      AND missing > 0
+      AND l.retry < 5
+    WHERE l.node_id = ?
+    ORDER BY l.node_id, missing desc'''
+
 
     def __init__(self, dbName):
         self.dbName = dbName
@@ -47,8 +60,21 @@ class DatabaseMissing(object):
         if self.connected == True:
             #self.cursor.close()
             self.connection.close()
-            
             print "closing connection"
+
+    def nodeMissing(self, node, incrementRetries=True):
+        # sqlite connections can only be used from the same threads they are established from
+        if self.connected == False:
+            self.connected == True
+            # raises sqlite3 exceptions
+            self.connection = sqlite3.connect(self.dbName)
+            #self.cursor = self.connection.cursor()
+        # sort the flash table by cookie values (ascending)
+        # and find missing segments by comparing lengths and cookies
+        self.connection.execute(DatabaseMissing.SORT_COOKIE_SQL)
+        results = self.connection.execute(DatabaseMissing.MISSING_ORDER_SIZE_NODE_SQL, (node,))
+        return results.fetchone()
+        
 
     def findMissing(self, incrementRetries=True):
         
@@ -104,11 +130,11 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         dbName = sys.argv[1]
     dbm = DatabaseMissing(dbName)
-    (toReq, allMissing) = dbm.findMissing(False)
-    for m in allMissing:
-        print "GAP", m
-    for m in toReq:
-        print "REQ",m
-
-
-
+    if len(sys.argv) <= 2:
+        (toReq, allMissing) = dbm.findMissing(False)
+        for m in allMissing:
+            print "GAP", m
+        for m in toReq:
+            print "REQ",m
+    else:
+        print dbm.nodeMissing(int(sys.argv[2]), False)
