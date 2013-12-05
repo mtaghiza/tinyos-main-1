54cf0e2b037cf23063e6ddc2898efa0474b78c83
Marcus Chang <mchang@cs.jhu.edu>
2013-12-05 13:28:01 -0500

    Dashboard.py  Fixed cross-platform bug. : is not allowed in Windows file names."

--- apps/breakfast/tools/Life/Dashboard.py
+++ apps/breakfast/tools/Life/Dashboard.py
@@ -7,6 +7,10 @@ if __name__ == "__main__":
     logDir ='logs'
     if not os.path.isdir(logDir):
         os.mkdir(logDir)
-    f = open(os.path.join(logDir, "%s.log"%(datetime.datetime.now())), 'w')
+        
+    now = datetime.datetime.now()
+    now_str = now.strftime("%Y%m%dT%H%M%S")
+    
+    f = open(os.path.join(logDir, "%s.log"%now_str), 'w')
     subprocess.call("python DashboardInternal.py", stdout=f,
       stderr=subprocess.STDOUT, shell=True)
